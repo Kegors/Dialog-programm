8a4c0f8ece2a5a9d3663a56a23c1f5614a72d66d
Kegors <64145189+Kegors@users.noreply.github.com>
2020-06-04 14:01:11 +0700

    Delete ButtonProcessor.py

--- ButtonProcessor.py
+++
@@ -1,9 +0,0 @@
-import Button
-
-
-def IsButtonPressed(button, x, y):
-#Check, is button pressed
-    if y >= button.Y and y <= button.Y + button.Height and x >= button.X  and x <= button.X + button.Width:
-        return True
-    else:
-        return False
